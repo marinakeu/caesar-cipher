2ddfb20b1fd328a56a6c129875b4e33ac13a5add
marinakeu <marina_keunecke@hotmail.com>
2019-01-22 17:38:01 -0200

    inclui funções para esconder e aparecer divs mas está como comentário pq não funcionou

--- src/cipher.js
+++ src/cipher.js
@@ -1,14 +1,31 @@
 window.cipher = {
   // ... 
-  var initialPage = document.getElementById("pagina-inicial");
-  var cipherPage = document.getElementById("pagina-cifrar");
-  var decodePage = document.getElementById("pagina-decifrar");
-  var messsagePage = document.getElementById("pagina-mensagem");
-
-  initialPage.className = "show";
-  cipherPage.className = "hide";
-  decodePage.className = "hide";
-  messsagePage.className = "hide";
-
+  
   
 };
+
+/* Algum problema não chama a função da Initial Page
+var initialPage = document.getElementById("pagina-inicial");
+var cipherPage = document.getElementById("pagina-cifrar");
+var decodePage = document.getElementById("pagina-decifrar");
+var messagePage = document.getElementById("pagina-mensagem");
+
+cipherPage.className = "hide";
+decodePage.className = "hide";
+messagePage.className = "hide";
+
+function showCipherPage() {
+  initialPage.className = "hide";
+  cipherPage.className = cipherPage.className.replace("hide" , "show");
+}
+
+function showDecodePage() {
+  initialPage.className = "hide";
+  decodePage.className = decodePage.className.replace("hide" , "show");
+  }
+  */
+
+
+
+
+
