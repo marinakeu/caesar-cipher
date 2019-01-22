0d586fd8a7742cec63227171e81e35112b77d52c
marinakeu <marina_keunecke@hotmail.com>
2019-01-22 17:03:55 -0200

    inclui classes no CSS e declarei variáveis para esconder divs no começo

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,14 @@
 window.cipher = {
   // ... 
+  var initialPage = document.getElementById("pagina-inicial");
+  var cipherPage = document.getElementById("pagina-cifrar");
+  var decodePage = document.getElementById("pagina-decifrar");
+  var messsagePage = document.getElementById("pagina-mensagem");
+
+  initialPage.className = "show";
+  cipherPage.className = "hide";
+  decodePage.className = "hide";
+  messsagePage.className = "hide";
+
+  
 };
