60fa771c3f3b9d05f15f2924c5822894344fd4ab
marinakeu <marina_keunecke@hotmail.com>
2019-01-24 15:42:37 -0200

    comecei encode e pagina final não funciona botão

--- src/cipher.js
+++ src/cipher.js
@@ -1,23 +1,33 @@
-var initialPage = document.getElementById("pagina-inicial");
-var cipherPage = document.getElementById("pagina-cifrar");
-var decodePage = document.getElementById("pagina-decifrar");
-var messagePage = document.getElementById("pagina-mensagem");
+var initialPage = document.getElementById('pagina-inicial');
+var cipherPage = document.getElementById('pagina-cifrar');
+var decodePage = document.getElementById('pagina-decifrar');
+var messagePage = document.getElementById('pagina-mensagem');
 
-cipherPage.className = "hide";
-decodePage.className = "hide";
-messagePage.className = "hide";
+cipherPage.className = 'hide';
+decodePage.className = 'hide';
+messagePage.className = 'hide';
 
-function showCipherPage() {
-  initialPage.className = "hide";
-  cipherPage.className = cipherPage.className.replace("hide" , "show");
+function showInitialPage() {
+  messagePage.className = messagePage.className.replace('hide' , 'show');
+  initialPage.className = initialPage.className.replace('show' , 'hide');
+}
+
+function showEncodePage() {
+  initialPage.className = 'hide';
+  cipherPage.className = cipherPage.className.replace('hide' , 'show');
 }
 
 function showDecodePage() {
-  initialPage.className = "hide";
-  decodePage.className = decodePage.className.replace("hide" , "show");
+  initialPage.className = 'hide';
+  decodePage.className = decodePage.className.replace('hide' , 'show');
   }
   
+function encode() {
+  cipherPage.className = cipherPage.className.replace('show' , 'hide');
+  messagePage.className = messagePage.className.replace('hide' , 'show');
+  let messageToEncode = document.getElementById('mensagem-cifrar').nodeValue;
 
+}
 
 
 
