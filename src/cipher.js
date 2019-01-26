569d5a70382dd434b7b300c133d36480b79cceba
marinakeu <marina_keunecke@hotmail.com>
2019-01-26 10:44:52 -0200

    inclui if para cifrar/decifrar minusculas e outros caracteres - tudo ok

--- src/cipher.js
+++ src/cipher.js
@@ -30,11 +30,22 @@ function encode() {
 
   for( i = 0; i < messageToEncode.length; i++) {
   messageLetterCode[i] = messageToEncode.charCodeAt(i);
+
+  if(messageLetterCode[i] >= 65 && messageLetterCode[i]<= 90) {
   let includeOffset = ((messageLetterCode[i] - 65 + encodeOffSet)%26) + 65;
   newMessage = newMessage + String.fromCharCode(includeOffset);
   console.log(messageLetterCode);
   console.log(includeOffset);
   console.log(newLetters);
+  } 
+  else if(messageLetterCode[i] >= 97 && messageLetterCode[i] <= 122) {
+  let includeOffset = ((messageLetterCode[i] - 97 + encodeOffSet)%26) + 97;
+  newMessage = newMessage + String.fromCharCode(includeOffset);
+  }
+  else {
+  newMessage = newMessage + String.fromCharCode(messageLetterCode[i]);
+  }
+
     } 
 
   let printNewMessage = document.getElementById('mensagem-nova');
@@ -54,12 +65,23 @@ function decode() {
 
   for( i = 0; i < messageToEncode.length; i++) {
   messageLetterCode[i] = messageToEncode.charCodeAt(i);
+
+  if(messageLetterCode[i] >= 65 && messageLetterCode[i]<= 90) {
   let includeOffset = ((messageLetterCode[i] - 65 - encodeOffSet)%26) + 65;
   newMessage = newMessage + String.fromCharCode(includeOffset);
   console.log(messageLetterCode);
   console.log(includeOffset);
   console.log(newLetters);
-    } 
+  }
+  else if(messageLetterCode[i] >= 97 && messageLetterCode[i] <= 122) {
+  let includeOffset = ((messageLetterCode[i] - 97 - encodeOffSet)%26) + 97;
+  newMessage = newMessage + String.fromCharCode(includeOffset);
+  }
+  else {
+  newMessage = newMessage + String.fromCharCode(messageLetterCode[i]);
+  }
+  
+  } 
 
     let printNewMessage = document.getElementById('mensagem-nova');
    return printNewMessage.innerHTML = newMessage;
