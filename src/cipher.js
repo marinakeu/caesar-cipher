a51e3f2dd8790197382ec90beb3be6c3ebf5b761
marinakeu <marina_keunecke@hotmail.com>
2019-01-28 17:23:11 -0200

    arrumei hacker edition - caracteres especiais

--- src/cipher.js
+++ src/cipher.js
@@ -36,14 +36,25 @@ function encode() {
 
   for( i = 0; i < messageToEncode.length; i++) {
   messageLetterCode[i] = messageToEncode.charCodeAt(i);
-  let includeOffset = (messageLetterCode[i] - 32 + encodeOffSet%223 + 223)%223 + 32;
+
+  if(messageLetterCode[i] >= 65 && messageLetterCode[i]<= 90) {
+  let includeOffset = (messageLetterCode[i] - 65 + encodeOffSet%26 + 26)%26 + 65;
+  //let includeOffset = (messageLetterCode[i] - 32 + encodeOffSet%223 + 223)%223 + 32;
   newMessage = newMessage + String.fromCharCode(includeOffset);
   console.log(messageLetterCode);
   console.log(encodeOffSet);
   console.log(includeOffset);
   console.log(newMessage);
-   } 
+  } 
+  else if(messageLetterCode[i] >= 97 && messageLetterCode[i] <= 122) {
+    let includeOffset = (messageLetterCode[i] - 97 + encodeOffSet%26 + 26)%26 + 97;
+    newMessage = newMessage + String.fromCharCode(includeOffset);
+  }
+  else {
+    newMessage = newMessage + String.fromCharCode(messageLetterCode[i]);
+  }
 
+  }
   let printNewMessage = document.getElementById('mensagem-nova-cifrada');
    return printNewMessage.innerHTML = newMessage;
 
@@ -61,14 +72,24 @@ function decode() {
   for( i = 0; i < messageToDecode.length; i++) {
   messageLetterCode[i] = messageToDecode.charCodeAt(i);
   console.log(decodeOffSet);
-  let includeOffset = (messageLetterCode[i] - 32 - (decodeOffSet%223) + 223)%223 + 32;
-  newMessage = newMessage + String.fromCharCode(includeOffset);
-  console.log(messageLetterCode);
-  console.log(decodeOffSet);
-  console.log(includeOffset);
-  console.log(newMessage);
+  
+  if(messageLetterCode[i] >= 65 && messageLetterCode[i]<= 90) {
+    let includeOffset = (messageLetterCode[i] - 65 - decodeOffSet%26 + 26)%26 + 65;
+    newMessage = newMessage + String.fromCharCode(includeOffset);
+    console.log(messageLetterCode);
+    console.log(decodeOffSet);
+    console.log(includeOffset);
+    console.log(newMessage);
+    } 
+    else if(messageLetterCode[i] >= 97 && messageLetterCode[i] <= 122) {
+      let includeOffset = (messageLetterCode[i] - 97 - decodeOffSet%26 + 26)%26 + 97;
+      newMessage = newMessage + String.fromCharCode(includeOffset);
+    }
+    else {
+      newMessage = newMessage + String.fromCharCode(messageLetterCode[i]);
+    }
   } 
-
+console.log(newMessage);
     let printNewMessage = document.getElementById('mensagem-nova-decifrada');
    return printNewMessage.innerHTML = newMessage;
 
