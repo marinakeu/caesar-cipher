2f95112b773918eac3f2a1ff8cc39b730ffde40f
marinakeu <marina_keunecke@hotmail.com>
2019-01-29 18:01:53 -0200

    melhorei css

--- src/cipher.js
+++ src/cipher.js
@@ -1,111 +1,95 @@
 var initialPage = document.getElementById('pagina-inicial');
-var cipherPage = document.getElementById('pagina-cifrar');
-var decodePage = document.getElementById('pagina-decifrar');
-var cipherMessage = document.getElementById('mensagem-cifrada');
-var decodeMessage = document.getElementById('mensagem-decifrada');
+var cipherPage = document.getElementById('cifrar');
+var newMessage = document.getElementById('mensagem-cifrada');
 
 cipherPage.className = 'hide';
-decodePage.className = 'hide';
-cipherMessage.className = 'hide';
-decodeMessage.className = 'hide';
+newMessage.className = 'hide';
 
-function showEncodePage() {
+function showCipherPage() {
   initialPage.className = 'hide';
-  decodeMessage.className = 'hide';
-  decodePage.className = 'hide';
-  cipherPage.className = 'show';
+  cipherPage.className = cipherPage.className.replace('hide', 'display');
+  newMessage.className = newMessage.className.replace('hide', 'display');
 }
-
-function showDecodePage() {
-  initialPage.className = 'hide';
-  cipherMessage.className = 'hide';
-  cipherPage.className = 'hide';
-  decodePage.className = 'show';
-  }
 
 function saveEncode() {
-  cipherMessage.className = 'show';
- const encodeOffset = parseInt(document.getElementById('deslocamento-cifrar').value);
-  const messageToEncode = document.getElementById('mensagem-cifrar').value;
+  const encodeOffset = parseInt(document.getElementById('deslocamento').value);
+  const messageToEncode = document.getElementById('mensagem').value;
   let cipher = encode(messageToEncode, encodeOffset);
-  
-  let printNewMessage = document.getElementById('mensagem-nova-cifrada');
+
+  let printNewMessage = document.getElementById('mensagem-nova');
   printNewMessage.innerHTML = cipher;
 }
-  
+
 function encode(messageToEncode, encodeOffset) {
-  let messageLetterCode  = [];
+  let messageLetterCode = [];
   let newMessage = '';
 
-  for( i = 0; i < messageToEncode.length; i++) {
-  messageLetterCode[i] = messageToEncode.charCodeAt(i);
+  for (i = 0; i < messageToEncode.length; i++) {
+    messageLetterCode[i] = messageToEncode.charCodeAt(i);
 
-  if(messageLetterCode[i] >= 65 && messageLetterCode[i]<= 90) {
-  let includeOffset = (messageLetterCode[i] - 65 + encodeOffset%26 + 26)%26 + 65;
-  newMessage = newMessage + String.fromCharCode(includeOffset);
-  console.log(messageLetterCode);
-  console.log(encodeOffset);
-  console.log(includeOffset);
-  console.log(newMessage);
-  } 
-  else if(messageLetterCode[i] >= 97 && messageLetterCode[i] <= 122) {
-    let includeOffset = (messageLetterCode[i] - 97 + encodeOffset%26 + 26)%26 + 97;
-    newMessage = newMessage + String.fromCharCode(includeOffset);
-  }
-  else {
-    newMessage = newMessage + String.fromCharCode(messageLetterCode[i]);
-  }
+    if (messageLetterCode[i] >= 65 && messageLetterCode[i] <= 90) {
+      let includeOffset = (messageLetterCode[i] - 65 + encodeOffset % 26 + 26) % 26 + 65;
+      newMessage = newMessage + String.fromCharCode(includeOffset);
+      console.log(messageLetterCode);
+      console.log(encodeOffset);
+      console.log(includeOffset);
+      console.log(newMessage);
+    }
+    else if (messageLetterCode[i] >= 97 && messageLetterCode[i] <= 122) {
+      let includeOffset = (messageLetterCode[i] - 97 + encodeOffset % 26 + 26) % 26 + 97;
+      newMessage = newMessage + String.fromCharCode(includeOffset);
+    }
+    else {
+      newMessage = newMessage + String.fromCharCode(messageLetterCode[i]);
+    }
 
   }
   return newMessage;
 
 }
 
-function saveDecode(){
-  decodeMessage.className = 'show';
-  const decodeOffSet = parseInt(document.getElementById('deslocamento-decifrar').value);
-  const messageToDecode = document.getElementById('mensagem-decifrar').value;
+function saveDecode() {
+  const decodeOffSet = parseInt(document.getElementById('deslocamento').value);
+  const messageToDecode = document.getElementById('mensagem').value;
   let decipher = decode(messageToDecode, decodeOffSet);
 
-  let printNewMessage = document.getElementById('mensagem-nova-decifrada');
+  let printNewMessage = document.getElementById('mensagem-nova');
   printNewMessage.innerHTML = decipher;
 }
 
 function decode(messageToDecode, decodeOffSet) {
-  let messageLetterCode  = [];
+  let messageLetterCode = [];
   let newMessage = '';
 
-  for( i = 0; i < messageToDecode.length; i++) {
-  messageLetterCode[i] = messageToDecode.charCodeAt(i);
-  console.log(decodeOffSet);
-  
-  if(messageLetterCode[i] >= 65 && messageLetterCode[i]<= 90) {
-    let includeOffset = (messageLetterCode[i] - 65 - decodeOffSet%26 + 26)%26 + 65;
-    newMessage = newMessage + String.fromCharCode(includeOffset);
-    console.log(messageLetterCode);
+  for (i = 0; i < messageToDecode.length; i++) {
+    messageLetterCode[i] = messageToDecode.charCodeAt(i);
     console.log(decodeOffSet);
-    console.log(includeOffset);
-    console.log(newMessage);
-    } 
-    else if(messageLetterCode[i] >= 97 && messageLetterCode[i] <= 122) {
-      let includeOffset = (messageLetterCode[i] - 97 - decodeOffSet%26 + 26)%26 + 97;
+
+    if (messageLetterCode[i] >= 65 && messageLetterCode[i] <= 90) {
+      let includeOffset = (messageLetterCode[i] - 65 - decodeOffSet % 26 + 26) % 26 + 65;
+      newMessage = newMessage + String.fromCharCode(includeOffset);
+      console.log(messageLetterCode);
+      console.log(decodeOffSet);
+      console.log(includeOffset);
+      console.log(newMessage);
+    }
+    else if (messageLetterCode[i] >= 97 && messageLetterCode[i] <= 122) {
+      let includeOffset = (messageLetterCode[i] - 97 - decodeOffSet % 26 + 26) % 26 + 97;
       newMessage = newMessage + String.fromCharCode(includeOffset);
     }
     else {
       newMessage = newMessage + String.fromCharCode(messageLetterCode[i]);
     }
-  } 
-console.log(newMessage);
-    
-return newMessage;
+  }
+  console.log(newMessage);
+
+  return newMessage;
 
 }
 
 function showInitialPage() {
-  cipherPage.className = 'hide';
-  decodePage.className = 'hide';
-  cipherMessage.className = 'hide';
-  decodeMessage.className = 'hide';
+  cipherPage.className = cipherPage.className.replace('display', 'hide');
+  newMessage.className = newMessage.className.replace('display', 'hide');
   initialPage.className = 'show';
 }
 
