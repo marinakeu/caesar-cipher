86758f62b4dcc997332cda6d077130debaa4acf7
marinakeu <marina_keunecke@hotmail.com>
2019-01-29 15:35:14 -0200

    implementei função de testes em tests - está ok

--- src/tests.js
+++ src/tests.js
@@ -1,3 +1,13 @@
-function checkCipher(calculado, esperao){
-    
+function checkCipher(calculado, esperado){
+if (calculado === esperado) {
+    console.log('Eba!');
+    return true;
+} else {
+    console.log('Deu erro ' + calculado + 'não é igual a ' + esperado);
+    return false;
 }
+}
+
+checkCipher(encode('a', 1), 'b');
+
+
